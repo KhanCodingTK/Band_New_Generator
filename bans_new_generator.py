edcdbe1fe7ee8fae6d22da6c352953ee55840f37
Taha <khancodingtk@gmail.com>
2022-05-29 13:50:47 -0700

    Work Complete

---
+++ bans_new_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band New Generator.")
+city_name = (input("What's name of the city you grew up in?\n"))
+color_name = (input("What's your favorate color?\n"))
+print("Your band name could be " + city_name + " " + color_name)
